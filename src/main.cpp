e0e344250f223be5535257e40719ad9a44fe83e1
Doridian <git@doridian.net>
2023-01-18 23:30:26 -0800

    Initial

---
+++ src/main.cpp
@@ -0,0 +1,130 @@
+#define ENABLE_GxEPD2_GFX 0
+#include <GxEPD2_7C.h>
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include "config.h"
+#include "config_private.h"
+
+#define GxEPD2_DISPLAY_CLASS GxEPD2_7C
+#define GxEPD2_DRIVER_CLASS GxEPD2_565c
+
+// Set page height to 1 to ensure we allocate the least overhead possible, we don't need pages
+GxEPD2_DISPLAY_CLASS<GxEPD2_DRIVER_CLASS, 1> display(GxEPD2_DRIVER_CLASS(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
+
+// Calculate bytes needed for buffer (+1 to force round up)
+const uint32_t display_buffer_size = ((display.height() * display.width()) + 1) / 2;
+#define buffer_malloc ps_malloc
+uint8_t* display_buffer;
+
+// Pixel format: 4-bit-per-pixel, indexed colors: 
+#define COLOR_BLACK 0x00
+#define COLOR_WHITE 0x01
+#define COLOR_GREEN 0x02
+#define COLOR_BLUE 0x03
+#define COLOR_RED 0x04
+#define COLOR_YELLOW 0x05
+#define COLOR_ORANGE 0x06
+
+WiFiClientSecure client;
+RTC_DATA_ATTR char token[128];
+
+void setup() {
+  Serial.begin(115200);
+
+  SPI.end();
+  SPI.begin(EPD_CLK, EPD_DOUT, EPD_DIN, EPD_CS);
+  display.init(115200);
+
+  display_buffer = (uint8_t*)buffer_malloc(display_buffer_size);
+
+  Serial.println("Init complete! Connecting WiFi...");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Failed!");
+    return;
+  }
+
+  client.setCACert(IMAGE_URL_CA);
+
+  Serial.println("WiFi connected!");
+}
+
+static bool drawNextImage() {
+  token[sizeof(token) - 1] = 0;
+  Serial.printf("Downloading image with token \"%s\"...", token); Serial.println();
+
+  if (!client.connect(IMAGE_URL_HOST, 443)) {
+    Serial.println("Connection failed!");
+    return false;
+  }
+
+  client.printf("GET " IMAGE_URL " HTTP/1.1", token); client.println();
+  client.println("Host: " IMAGE_URL_HOST);
+  client.println("Connection: close");
+  client.println();
+
+  while (client.connected()) {
+    const String line = client.readStringUntil('\n');
+
+    const int idx = line.indexOf(":");
+    if (idx > 0) {
+      String headerName = line.substring(0, idx);
+      headerName.toLowerCase();
+      if (headerName == "token") {
+        String headerValue = line.substring(idx + 1);
+        headerValue.trim();
+        strncpy(token, headerValue.c_str(), sizeof(token));
+        Serial.printf("Got new token: %s", token); Serial.println();
+      }
+    }
+
+    if (line == "\r") {
+      Serial.println("Headers received. Fetching body...");
+      break;
+    }
+  }
+
+  uint32_t buffer_load_left = display_buffer_size;
+  uint8_t* buffer_load = display_buffer;
+  while ((client.connected() || client.available()) && buffer_load_left > 0) {
+    int len = client.read(buffer_load, buffer_load_left);
+    if (len <= 0) {
+      continue;
+    }
+    buffer_load += len;
+    buffer_load_left -= len;
+  }
+
+  const int data_left = client.available();
+  client.stop();
+
+  if (data_left) {
+    Serial.print("Failed to download image! Too big: ");
+    Serial.println(data_left);
+    return false;
+  }
+
+  if (buffer_load_left > 0) {
+    Serial.print("Failed to download image! Too small! Missing bytes: ");
+    Serial.println(buffer_load_left);
+    return false;
+  }
+
+  Serial.println("Downloading complete. Drawing image...");
+  display.drawNative(display_buffer, 0, 0, 0, display.width(), display.height(), false, false, false);
+  display.hibernate();
+  Serial.println("All done!");
+  return true;
+}
+
+void loop() {
+  if (!drawNextImage()) {
+    PaperESPSleep(PAPER_ESP_ERROR_SLEEP);
+    return;
+  }
+
+  PaperESPSleep(PAPER_ESP_OK_SLEEP);
+}
---
+++ tools/convert.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+from argparse import ArgumentParser
+
+"""
+1.	Black	0b000	0x0	0,0,0
+2.	White	0b001	0x1	255,255,255
+3.	Green	0b010	0x2	0,255,0
+4.	Blue	0b011	0x3	0,0,255
+5.	Red	0b100	0x4	255,0,0
+6.	Yellow	0b101	0x5	255,255,0
+7.	Orange	0b110	0x6	255,128,0
+"""
+
+PALLETTE = [
+    0, 0, 0,
+    255, 255, 255,
+    0, 255, 0,
+    0, 0, 255,
+    255, 0, 0,
+    255, 255, 0,
+    255, 128, 0,
+]
+
+TARGET_WIDTH = 600
+TARGET_HEIGHT = 448
+TARGET_RATIO = TARGET_WIDTH / TARGET_HEIGHT
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("src", help="Source image")
+    parser.add_argument("dst", help="Destination file")
+    parser.add_argument("--save-intermediate", help="Save intermediate file", default="")
+    args = parser.parse_args()
+
+    tmp_byte = bytearray()
+
+    print("Loading image...")
+    with Image.open(args.src) as img:
+        palette_img = Image.new("P", (1, 1))
+        palette_img.putpalette(PALLETTE)
+
+        print("Cropping image to target ratio...")
+        image_ratio = img.width / img.height
+        if image_ratio > TARGET_RATIO:
+            target_width = img.height * TARGET_RATIO
+            img = img.crop(((img.width - target_width) / 2, 0, (img.width + target_width) / 2, img.height))
+        elif image_ratio < TARGET_RATIO:
+            target_height = img.width * TARGET_RATIO
+            img = img.crop((0, (img.height - target_height) / 2, img.width, (img.height + target_height) / 2))
+
+        print("Resizing image...")
+        img = img.resize((TARGET_WIDTH, TARGET_HEIGHT))
+
+        print("Quantizing image...")
+        processed_img = img.quantize(palette=palette_img, dither=Image.Dither.FLOYDSTEINBERG)
+
+    print("Converting to binary...")
+
+    for y in range(0, TARGET_HEIGHT):
+        for x in range(0, TARGET_WIDTH, 2):
+            tmp_byte.append(processed_img.getpixel((x, y)) | (processed_img.getpixel((x + 1, y)) << 4))
+
+    if args.save_intermediate:
+        print("Saving intermediate file...")
+        processed_img.save(args.save_intermediate)
+
+    print("Saving binary file...")
+    with open(args.dst, "wb") as out:
+        out.write(tmp_byte)
+
+    print("Done!")
+
+if __name__ == "__main__":
+    main()
