4df6fb7c1d5900507fabdf81a48f9c22f0cbb5ff
Doridian <git@doridian.net>
2023-01-23 19:36:09 -0800

    color tweaks

--- include/config.h
+++ include/config.h
@@ -45,8 +45,8 @@ emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n\
 
 #define PaperESPSleep delayMicroseconds
 //#define PaperESPSleep ESP.deepSleep
-//#define PAPER_ESP_OK_SLEEP (1 * 60 * 60 * 1e6)
-#define PAPER_ESP_OK_SLEEP (60 * 1e6)
+#define PAPER_ESP_OK_SLEEP (1 * 60 * 60 * 1e6)
+//#define PAPER_ESP_OK_SLEEP (60 * 1e6)
 #define PAPER_ESP_ERROR_SLEEP (60 * 1e6)
 
 #define PAPER_POWER_PIN     21
